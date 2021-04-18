8b8779abf14c193fcf061689ac1b94b761c60639
Dhruvin <dhruvinp895@gmail.com>
2021-04-18 14:48:35 -0500

    WIP Oauth2 login with TD

--- stock-viewer/src/components/Login/Login.js
+++ stock-viewer/src/components/Login/Login.js
@@ -1,7 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Login.module.css";
-import {Button, Container, Row, Col} from 'react-bootstrap';
+import { Button, Container, Row, Col } from "react-bootstrap";
+import OauthPopup from "react-oauth-popup";
+import {completeOauthUrl} from '../../key';
+
+// TODO: Work on getting the code from the url and figure out why onCode does not trigger on successfull login
+const onCode = (code, params) => {
+  console.log("wooooo a code", code);
+  console.log(
+    "alright! the URLSearchParams interface from the popup url",
+    params
+  );
+};
+
+const onClose = (data) => console.log(data);
 
 const Login = () => (
   <div className={styles.Login} data-testid="Login">
@@ -9,7 +22,15 @@ const Login = () => (
     <Container>
       <Row className="justify-content-md-center">
         <Col>
-          <Button></Button>
+        <Button>
+            <OauthPopup
+              url = {completeOauthUrl}
+              onCode = {onCode}
+              onClose = {onClose}
+            >
+              <div>Login to TD Ameritrade</div>
+            </OauthPopup>
+          </Button>
         </Col>
       </Row>
     </Container>
